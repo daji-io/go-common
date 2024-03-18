49ab282eb41e4cff5dc61a46b695a8e3e638d30e
Cameron Mukherjee <cameron@hexploits.com>
2024-03-18 12:20:23 +0000

    feat: added a log when schema migration is occurring.

--- util/migrate/migrate.go
+++ util/migrate/migrate.go
@@ -6,9 +6,11 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/sirupsen/logrus"
 )
 
 func MigrateConfigs(db *sql.DB, schema string) error {
+	logrus.Infof("migrating schemas: %s", schema)
 	driver, err := postgres.WithInstance(db, &postgres.Config{
 		MigrationsTable: fmt.Sprintf("%s-migration", schema),
 	})
